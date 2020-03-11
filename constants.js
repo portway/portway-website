08af64d122c75ea5028a674cd3e59bbb4146f652
Jay Contonio <jay@bonkeybong.com>
2020-03-11 12:28:42 -0700

    Removing dev value

--- constants.js
+++ constants.js
@@ -2,6 +2,5 @@ module.exports = {
   LINK_LOGIN: 'https://portway.app/',
   LINK_SIGNUP: 'https://portway.app/sign-up',
   LINK_REGISTRATION_FORM: 'https://portway.app/sign-up/registration',
-  // FORM_SUBMISSION_ACTION_URL: 'https://portway.app/squishymuffins'
-  FORM_SUBMISSION_ACTION_URL: 'http://localhost:3000/squishymuffins'
+  FORM_SUBMISSION_ACTION_URL: 'https://portway.app/squishymuffins',
 }
