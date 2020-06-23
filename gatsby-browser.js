b7e530afebfdafc01a008fef3f05cc383c80e804
Jay Contonio <jay@conton.io>
2020-06-23 14:18:46 -0700

    Adding manual tracking

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,3 +7,8 @@
 // You can delete this file if you're not using it
 import './src/css/main.scss'
 import './src/css/shared.scss'
+
+export const onRouteUpdate = ({ location, prevLocation }) => {
+  // Manually track with GoSquared
+  _gs('GSN-113617-U');
+}
