eb18d9eec60cb465180633a80152888234e2589d
Jay Contonio <jay@conton.io>
2020-07-10 09:13:40 -0700

    Adding 404 items

--- src/html.js
+++ src/html.js
@@ -12,20 +12,9 @@ export default function HTML(props) {
         <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no" />
         <meta name="robots" content="all" />
-        <meta name="author" content="BonkeyϟBong, LLC" />
         <meta name="theme-color" content="#F2F2F2" />
+        <link rel="author" href="humans.txt" />
         {props.headComponents}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
-              arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
-              d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
-              insertBefore(d,q)}(window,document,'script','_gs');
-              _gs('GSN-113617-U');
-              _gs('set', 'anonymizeIP', true);
-            `,
-          }} />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
